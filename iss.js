43ba9b8b265439b83ddf3915bfeadac195ac98a4
Jerome <101889450+jeromealmir@users.noreply.github.com>
2022-11-23 22:32:43 -0800

    implement fetchMyIp function that makes an api request to get ip address

---
+++ iss.js
@@ -0,0 +1,20 @@
+const request = require('request');
+
+const fetchMyIP = (callback) => {
+  request('https://api.ipify.org?format=json', (error, response, body) => {
+
+    if (error) return callback(error, null);
+
+    const ip = JSON.parse(body).ip;
+
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+      
+    return callback(null, ip);
+  });
+};
+
+module.exports = {fetchMyIP};
